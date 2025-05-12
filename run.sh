a24352e87e0647fdf903b667a59601443907ca56
unknown <anniehuang921@gmail.com>
2025-05-12 14:31:21 +0800

    clone webvoyger rag

---
+++ run.sh
@@ -0,0 +1,12 @@
+python -u run.py \
+    --test_file ./data/arxiv_tasks.jsonl \
+    --api_key YOUR_OPENAI_API_KEY \
+    --api_model gpt-4o \
+    --max_iter 15 \
+    --max_attached_imgs 3 \
+    --temperature 0 \
+    --fix_box_color \
+    --seed 42 \
+    --window_width 1920 \
+    --window_height 1080 \
+    --pdf_path data/arXiv.pdf
